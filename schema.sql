c265b11e994bdcbe495ffb3a2a28590e0432029a
Ayobami1014 <amplumbingworks10@gmail.com>
2026-02-03 10:46:29 -0800

    Add database schema file

--- schema.sql
+++ schema.sql
@@ -1,2 +1,6 @@
--- Database schema for Sales Anaytics Relational SQL project
--- Include tables, relationships, and integrity constraints
+/*
+- Project: Sales Analytics Using Relational SQL
+- Author: Ayobami Edunjobi
+- Description: Defines the relational schema, constaints, and hierarchical relationships
+- Used for sales performance analysis.
+*/
