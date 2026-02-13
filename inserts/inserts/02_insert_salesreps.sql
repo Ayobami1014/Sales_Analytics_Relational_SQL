7629f921ede5a01ea523a085a78497fbb4f1ecdc
Ayobami1014 <amplumbingworks10@gmail.com>
2026-02-13 14:55:16 -0800

    insert initial salesreps data

---
+++ inserts/inserts/02_insert_salesreps.sql
@@ -0,0 +1,11 @@
+INSERT INTO Sales.Salesreps ( Empl_Id, Name, Age, Office_Id, Tittle, Hire_Date, Mgr_Id, Quota, Amount)
+VALUES (101, 'Dan Robert', 45, 11, 'Sales Reps', '1986-10-20', 104, 350000.00, 305673.00),
+       (102, 'Sue Smith', 48, 21, 'Sales Rep', '1986-12-10', 108, 175000.00, 474050.00),
+	   (103, 'Bob Smith', 33, 11, 'Sales Mgr', '1987-05-19', 106, 90000.00, 142594.00),
+	   (104, 'Bill Adams', 37, 13, 'Sales Rep', '1988-02-12', 110, 175000.00, 367911.00),
+	   (105, 'Sam Clark', 52, 11, 'Vp Sales', '1988-06-14',  NULL, 137500.00, 299912.00),
+	   (106, 'Nancy Angele', 49, 22, 'Sales Rep', '1988-11-14', 103, 150000.00, 186042.00),
+	   (107, 'Larry Fitch', 52, 21, 'Sales Rep', '1989-10-12', 101, 175000.00, 384877.00),
+	   (108, 'Mary Jones', 31, 11, 'Sales Rep', '1989-10-12', 107, 150000.00, 392725.00),
+	   (109, 'Tom Snyder', 41, 22,'Sales Rep', '1990-01-13', 102, 500000.00, 75985.00),
+	   (110,'Paul Cruz', 39, 12, 'Sales Mgr', '1989-02-15', 104, 275000.00, 286775.00);
